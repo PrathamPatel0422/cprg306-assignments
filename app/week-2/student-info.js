0fc9e6fe3cbb07d57037b1a574fa4e4de4d03468
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2025-09-17 21:52:31 -0600

    added week-2 files

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>John McKay, Pratham Patel, Manraj Virk</h1>
+            Render(display)
+            <Link href="https://github.com/PrathamPatel0422/cprg306-assignments">GitHub</Link>
+        </main>
+    )
+}

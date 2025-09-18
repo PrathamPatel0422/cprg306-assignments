296e6447365b964a114edaf984f880c57787094c
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2025-09-17 22:11:13 -0600

    test

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <main>
             <h1>Shopping List</h1>
-            <StudentInfo />
+            <StudentInfo /> 
         </main>
     );
 }

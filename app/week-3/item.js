77154e5834b86e1f479c772866d8bdb6d1d0ebbd
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2025-09-19 16:23:36 -0600

    created Item function

---
+++ app/week-3/item.js
@@ -0,0 +1,8 @@
+export default function Item(name, quantity, category) {
+    return (
+        <main>
+            <h1>{name}</h1>
+            <p>Buy {quanitity} in {category}</p>
+        </main>
+    );
+}

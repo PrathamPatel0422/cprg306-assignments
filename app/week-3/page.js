bf476a81684db98ae2076201a9c539703f6a6cd9
manrajsinghvirk <manrajsinghvirk4@gmail.com>
2025-09-19 16:25:49 -0600

    change the type of class to class name

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,7 +1,7 @@
 export default function Page() {    
     return (
         <main>
-            <h1 class="text-5xl text-center font-bold">Shopping List</h1>
+            <h1 className="text-5xl text-center font-bold">Shopping List</h1>
         </main>
     );
 } 

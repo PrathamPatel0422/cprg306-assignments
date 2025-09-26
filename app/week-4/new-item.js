dd7332c6ea7648e175d6afc7f493e15f0f8babea
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2025-09-26 15:42:18 -0600

    added styling

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -6,27 +6,17 @@ export default function newItem() {
     let [quantity, SetQuantity] = useState(1);
 
     const increment = () => {
-        if(quantity < 20){
-            SetQuantity(quantity + 1)
-        }
-        else{       
-            alert("You reached the max value (10)");
-        }
+        SetQuantity(quantity + 1)
     };
     const decrement = () => {
-        if(quantity > 1){
-            SetQuantity(quantity - 1)
-        }
-        else{       
-            alert("You reached the max value (10)");
-        }
+        SetQuantity(quantity - 1)
     };
 
     return(
-        <div>
-            <h1>Quantity: {quantity}</h1>
-            <button onClick={increment}>Increment + </button>
-            <button onClick={increment}>Decrement - </button>
+        <div className="my-1 flex gap-2">
+            <h1 className="text-3xl border-1 border-black text text-black flex justify-center px-2">{quantity}</h1>
+            <button disabled={quantity === 1 } onClick={decrement} className= "text-3xl disabled:bg-gray-700 disabled:hover:cursor-not-allowed rounded bg-blue-800  hover:bg-blue-600 hover:cursor-pointer px-2">-</button>
+            <button disabled={quantity === 20 } onClick={increment} className= "text-3xl disabled:bg-gray-700 disabled:hover:cursor-not-allowed rounded bg-blue-800  hover:bg-blue-600 hover:cursor-pointer px-2">+</button>
         </div>
     )
 }
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,7 +3,9 @@ import NewItem from "./new-item";
 export default function Page(){
  return(
     <main>
-        <NewItem />
+        <div className="flex bg-white justify-center mx-85 my-10">
+            <NewItem />
+        </div>
     </main>
     
  )

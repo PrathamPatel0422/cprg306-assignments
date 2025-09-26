cd18e9a95e3c53a25a4be8226c4ca0b9465079bd
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2025-09-26 15:49:16 -0600

    applied max width changes with flex box

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,7 +3,7 @@ import NewItem from "./new-item";
 export default function Page(){
  return(
     <main>
-        <div className="flex bg-white justify-center mx-85 my-10">
+        <div className="flex bg-white mx-auto my-auto justify-center max-w-35">
             <NewItem />
         </div>
     </main>

cc0160e0ede086e771ee0e5d8d81402f51688c60
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2025-10-10 16:46:56 -0600

    added category and item sorting

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -7,27 +7,31 @@ import { use, useState } from "react";
 
 export default function ItemList() {
   const [sortBy, setSortBy] = useState("name");
-  let itemArray = [...ItemData] 
+  let itemArray = [...ItemData];
 
+  if(sortBy === "name"){
+    itemArray.sort((a,b) => a.name.localeCompare(b.name));
+    
+  } 
+  else if(sortBy === "category"){
+    itemArray.sort((a,b) => a.category.localeCompare(b.category));
+  }
 
-  itemArray.sort((a,b) => a.name.localeCompare(b.name))
-return (
-  <main>
-    <div>
-      <button className="border border-black bg-white text-black rounded p-1 m-2" onClick={() => setSortBy("name")}> Sort by Name</button>
-      <button className="border border-black bg-white text-black rounded p-1 m-2" onClick={() => setSortBy("category")}> Sort by Category</button>
-      <h1>{sortBy}</h1>
-    </div>
-    <div>
-      <ul> 
-        {itemArray.map((item) => (
-          <li className="m-2 p-2">
-            <Item item={item} />           
-          </li>
-        ))}
-      </ul>
-    </div>
-  </main>
-);
+  return (
+    <main>
+      <div>
+        <button className="border border-black bg-white text-black rounded p-1 m-2" onClick={() => setSortBy("name")}> Sort by Name</button>
+        <button className="border border-black bg-white text-black rounded p-1 m-2" onClick={() => setSortBy("category")}> Sort by Category</button>
+      </div>
+      <div>
+        <ul> 
+          {itemArray.map((item) => (
+            <li className="m-2 p-2">
+              <Item item={item} />           
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
 }
-

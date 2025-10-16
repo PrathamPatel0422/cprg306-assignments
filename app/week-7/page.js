efa31703f479097a68aecf7a1da7758d8b27eac5
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2025-10-16 16:38:25 -0600

    updated main page.js to include week 7

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,11 +1,11 @@
-import Items from "./items.js";
+import Item from "./item.js";
 
 export default function Page() {    
     return (
         <main>
             <div className="mx-auto max-w-md p-4">
                 <h1 className="text-4xl font-bold">Shopping List</h1>
-                <Items />
+                <Item />
             </div>
             
         </main>
